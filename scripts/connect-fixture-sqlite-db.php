0332de074e864628bdf7628fe6bcb3a4d02b2248
balsama <adam.balsam@acquia.com>
2022-12-19 11:27:59 -0500

    debug

--- scripts/connect-fixture-sqlite-db.php
+++ scripts/connect-fixture-sqlite-db.php
@@ -14,6 +14,9 @@ $fs->copy(
     $defaultSettingsPath . '/databases/' . Helpers::FIXTURE_NAME,
 );
 
+echo "Current directory:\n";
+echo __DIR__;
+
 $fs->copy(
     $defaultSettingsPath . '/default.settings.fixturedb.php',
     $defaultSettingsPath . '/settings.fixturedb.php',
