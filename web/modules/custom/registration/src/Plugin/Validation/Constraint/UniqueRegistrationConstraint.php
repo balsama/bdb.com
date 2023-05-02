0e6466936c63101caa8073cc1f79edc39948708f
Adam Balsam <balsama@users.noreply.github.com>
2023-05-01 22:59:07 -0400

    Fixes #53 - Event report tweaks (#54)
    * Fixes #53 - Event report tweaks
    
    * phpcs ll

--- web/modules/custom/registration/src/Plugin/Validation/Constraint/UniqueRegistrationConstraint.php
+++ web/modules/custom/registration/src/Plugin/Validation/Constraint/UniqueRegistrationConstraint.php
@@ -16,7 +16,8 @@ use Symfony\Component\Validator\Constraint;
 class UniqueRegistrationConstraint extends Constraint
 {
     public $mismatch = 'User %username doesn\'t match the submitted Event Registration.';
-    public $notUnique = 'User %username has already created an Event report for the %eventname Event.';
+    public $notUnique = 'User %username has already created an Event Report for the %eventname Event.
+    You can find a link to the existing report on your account page under the "This user\'s content" section';
     public $notAuthenticated = <<<EOF
 You must be logged in to create an Event report for an Event.<br>
 <a href="/user/login">Log in</a> or <a href="/user/register">Create an Account</a>.
